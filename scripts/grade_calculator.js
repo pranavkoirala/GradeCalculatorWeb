f93a7f23eac20e3cb22a06a6c6d8eddf10bfe87c
Pranav <pranavkoirala7@protonmail.com>
2023-02-24 17:21:15 -0800

    non-accurate grade calculator

--- scripts/grade_calculator.js
+++ scripts/grade_calculator.js
@@ -1,45 +1,76 @@
-// Get the final grade from localStorage
-const finalGrade = localStorage.getItem("finalGrade");
-
-// Check if final grade exists in localStorage
-if (!finalGrade) {
-  // If it does not exist, tell the user to add some assignments and categories
-  const resultSection = document.querySelector("#result-section");
-  const result = document.querySelector("#result");
-  resultSection.style.display = "none";
-  result.textContent =
-    "Add some assignments and categories before calculating your grade.";
+// Get current grade from local storage and display it if it exists
+const currentGrade = localStorage.getItem("finalGrade");
+const currentGradeSpan = document.getElementById("current-grade");
+if (currentGrade) {
+  currentGradeSpan.textContent = currentGrade + "%";
 } else {
-  // If final grade exists, display the current grade to the user
-  const currentGrade = document.querySelector("#current-grade");
-  currentGrade.textContent = finalGrade;
+  currentGradeSpan.textContent = "N/A";
+  const resultSection = document.getElementById("result-section");
+  resultSection.style.display = "none";
+  const calculateBtn = document.getElementById("calculate-btn");
+  calculateBtn.disabled = true;
+  alert("Please add some assignments and categories first.");
 }
 
-// Calculate the required grade
-const calculateBtn = document.querySelector("#calculate-btn");
+// Calculate required points for each category
+function calculateRequiredPoints(targetGrade) {
+  const categories = JSON.parse(localStorage.getItem("categories"));
+  const assignments = JSON.parse(localStorage.getItem("assignments"));
+  let requiredPointsByCategory = {};
+
+  // Calculate total earned points and total possible points for each category
+  categories.forEach((category) => {
+    requiredPointsByCategory[category.name] = {
+      weight: category.weight,
+      earnedPoints: 0,
+      possiblePoints: 0,
+    };
+  });
+  assignments.forEach((assignment) => {
+    const categoryName = assignment.category;
+    requiredPointsByCategory[categoryName].earnedPoints +=
+      assignment.pointsEarned;
+    requiredPointsByCategory[categoryName].possiblePoints +=
+      assignment.pointsPossible;
+  });
+
+  // Calculate required points for each category
+  categories.forEach((category) => {
+    const categoryName = category.name;
+    const categoryWeight = category.weight;
+    const categoryRequiredPoints =
+      ((targetGrade / 100) *
+        requiredPointsByCategory[categoryName].possiblePoints -
+        requiredPointsByCategory[categoryName].earnedPoints) /
+      (categoryWeight / 100);
+    requiredPointsByCategory[categoryName].requiredPoints =
+      categoryRequiredPoints.toFixed(2);
+  });
+
+  return requiredPointsByCategory;
+}
+
+// Handle click on Calculate button
+const calculateBtn = document.getElementById("calculate-btn");
 calculateBtn.addEventListener("click", () => {
-  // Get the target grade from the input field
-  const targetGradeInput = document.querySelector("#target-grade-input");
-  const targetGrade = parseFloat(targetGradeInput.value);
+  const targetGradeInput = document.getElementById("target-grade-input");
+  const targetGrade = targetGradeInput.value;
+  const resultSection = document.getElementById("result-section");
+  const result = document.getElementById("result");
 
-  // Calculate the required grade
-  const requiredGrade = calculateRequiredGrade(finalGrade, targetGrade);
-
-  // Display the result to the user
-  const resultSection = document.querySelector("#result-section");
-  const result = document.querySelector("#result");
-  resultSection.style.display = "block";
-  result.textContent = `You need a ${requiredGrade}% on your remaining assignments to achieve your target grade.`;
-});
-
-// Calculate the required grade
-function calculateRequiredGrade(finalGrade, targetGrade) {
-  // Check if target grade is less than or equal to the final grade
-  if (targetGrade <= finalGrade) {
-    return 0;
+  if (isNaN(targetGrade) || targetGrade < 0 || targetGrade > 100) {
+    resultSection.style.display = "none";
+    alert("Please enter a valid target grade between 0 and 100.");
+  } else {
+    const requiredPointsByCategory = calculateRequiredPoints(targetGrade);
+    let resultText = "";
+    for (const [categoryName, categoryData] of Object.entries(
+      requiredPointsByCategory
+    )) {
+      const requiredPoints = categoryData.requiredPoints;
+      resultText += `You need ${requiredPoints} more points in the ${categoryName} category to achieve your target grade.<br>`;
+    }
+    result.innerHTML = resultText;
+    resultSection.style.display = "block";
   }
-
-  // Calculate the required grade
-  const requiredGrade = ((targetGrade - finalGrade) * 100) / (100 - finalGrade);
-  return Math.round(100 * requiredGrade) / 100;
-}
+});
